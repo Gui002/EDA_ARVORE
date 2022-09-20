9cd0459c02e9b9fa60d1fe318599dca8db87b8eb
ADMIN <ADMIN@LAPTOP-4763GG1P>
2022-09-20 10:46:03 +0200

    Arvore Binaria

--- src/arvore/Arvore.java
+++ src/arvore/Arvore.java
@@ -16,9 +16,6 @@ public class Arvore {
      */
     public static void main(String[] args) {
             System.out.println("Guilherme Jacob Panguana");
-            System.out.println("Guilherme Jacob Panguana");
-            System.out.println("Guilherme Jacob Panguana");
-            System.out.println("Guilherme Jacob Panguana");
-    }
+   }
     
 }
