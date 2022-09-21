891b51a3e15e0d5368ac371bce13812149ea8098
Gabriel <Gabriel@LAPTOP-F1QSJ5KH>
2022-09-21 14:33:51 +0200

    trabalhei com o metodo contem

--- Arvore/src/arvore/Arvore.java
+++ Arvore/src/arvore/Arvore.java
@@ -50,12 +50,63 @@ public class Arvore <Object extends Comparable> {//Comparable serve para compara
     }
     
     
+    public boolean contem(Object valor){
+        //Buscar o elemento na arvore
+        
+        Elemento <Object> atual = this.root;
+        Elemento <Object> pai = this.root;
+        boolean filho_esq = false;
+        
+        
+        if(root == null){ //Se a arvore estiver vazia, retorna false
+            return false;
+        }
+        while(atual.getValor()!=valor){ //enquanto não encontrou
+            pai = atual;
+            if(valor.compareTo(atual.getValor()) == -1){ //caminha para esquerda
+               atual = atual.getLeft(); 
+               filho_esq = true; // é filho a esquerda? Sim
+               
+            }else{ //caminha para direita
+                atual = atual.getRight();
+                filho_esq = false; // é filho a esquerda? Não
+            } 
+            if(atual == null)
+                return false; // encontrou uma folha -> sai;
+        }
+        return true;
+    }
+    
+    
+    
+    
+    
     public void inOrder(Elemento <Object> atual){
        
           if(atual!=null){
               inOrder(atual.getLeft());
               System.out.println(atual.getValor());
               inOrder(atual.getRight());
+          }     
+     }
+    
+      
+    public void preOrder(Elemento <Object> atual){
+       
+          if(atual!=null){  
+              System.out.println(atual.getValor());
+              preOrder(atual.getLeft());
+              preOrder(atual.getRight());
+          }     
+     }
+    
+      
+    public void postOrder(Elemento <Object> atual){
+       
+          if(atual!=null){  
+              postOrder(atual.getLeft());
+              postOrder(atual.getRight());
+              System.out.println(atual.getValor());
           }     
      }
 }
--- Arvore/src/arvore/Arvore_Binaria.java
+++ Arvore/src/arvore/Arvore_Binaria.java
@@ -16,8 +16,30 @@ public class Arvore_Binaria {
     public static void main(String[] args) {
         Arvore <Integer> arvore = new Arvore<Integer> ();
         arvore.insert(10);
+        arvore.insert(8);
         arvore.insert(5);
+        arvore.insert(9);
+        arvore.insert(7);
+        arvore.insert(18);
+        arvore.insert(13);
+        arvore.insert(20);
+        
+        System.out.println("\nIn-Order");
+        System.out.println("=======================================");
         arvore.inOrder(arvore.getRoot());
+        
+        
+        System.out.println("\nPre-Order");
+        System.out.println("=======================================");
+        arvore.preOrder(arvore.getRoot());
+        
+
+        System.out.println("\nPost-Order");
+        System.out.println("=======================================");
+        arvore.postOrder(arvore.getRoot());
+        
+        System.out.println("\nExiste 21? "+arvore.contem(21));
+        System.out.println("\nExiste 20? "+arvore.contem(20));
         
     }
     
